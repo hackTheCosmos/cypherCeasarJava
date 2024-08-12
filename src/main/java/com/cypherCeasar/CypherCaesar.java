b9c0990e51c8415c2dd2cc3ae2b8fb5d2cc719b5
HackTheCosmos44 <baileche.theo@gmail.com>
2024-08-12 22:00:48 +0200

    ok for ascii and latin encoding

--- src/main/java/com/cypherCeasar/CypherCaesar.java
+++ src/main/java/com/cypherCeasar/CypherCaesar.java
@@ -4,78 +4,150 @@ public class CypherCaesar {
 
 	public static void main(String[] args) {
 
-//		encrypt("salut ! Crypt moi, stp ?", 4);
-//		decrypt("Irgvctx qi", 4);
-		int temp = 199 ;
-		System.out.print((char)temp);
+		encrypt("Hey ! Comment va ? ça fait un bail ! 2022 non ?", 25);
+		decrypt("Gdx : Bnlldms uz 8 z ezhs tm azhk : +)++ mnm 8", 25);
+
 
 	}
-
 	
+//	static void latinToAscii(char[] latinsCharsArray, char latinChar, char asciiChar) {
+//		System.out.println(latinChar.replace(latinChar, asciiChar));
+//	}
+
 	/**
-	 * encrypt a message
+	 * encrypt message (works for ASCII and Latin encoding)
 	 * @param message
 	 * @param key
 	 */
 	static void encrypt(String message, int key) {
-		// on créer la variable qui va contenir le texte chiffré
-		String encryptMessage = "";
-
 		
-		//on parcours le texte à chiffrer
-		for (int i = 0; i < message.length(); i++) {
-			// on gère le cas où le caractère est un espace
-			if ((int)message.charAt(i) == 32) {
-				encryptMessage += (char)32;
-			//on gère le cas où l'on va trop loin dans la table ASCII (le max que l'on veut est à 122 : "z" minuscule)
-			} else if ( ((int)message.charAt(i) + key) > 122) {
-				int temp = ((int)message.charAt(i) + key) - 122;
-				encryptMessage += (char)(temp + 96);// les lettres minuscules commences à 97 dans la table ASCII
-				
-			//on gère le cas ou on n'est entre les lettres majuscules et les lettres minuscules dans la table ASCII (soit pour 90 < c < 97)
-			} else if (((int)message.charAt(i) + key) > 90 && (int)message.charAt(i) < 97) {
-				int temp = (((int)message.charAt(i) + key) - 90);
-				encryptMessage += (char)(temp + 64); //les lettres majuscules commences à 65 dans la table ASCII
-			} else if(((int)message.charAt(i) + key) > 32){
-				encryptMessage += (char)((int)message.charAt(i) + key);
+		String encryptMessage = "";
+		
+		for(int i = 0; i < message.length(); i++) {
+			
+			// for "space" (no change)	
+			if((int)message.charAt(i) == 32) {
+				encryptMessage += message.charAt(i);
+			//for special chars (after "space" and before uppercases)
+			}else if((int)message.charAt(i) >= 33 && (int)message.charAt(i) <= 64) {
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 64) {
+					encryptMessage += (char)(newAsciiCode - 32);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			}
+			//for uppercases
+			else if((int)message.charAt(i) >= 65 && (int)message.charAt(i) <= 90) {
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 90) {
+					encryptMessage += (char)(newAsciiCode - 26);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			//for special chars (after uppercases and before lowercases)
+			}else if((int)message.charAt(i) >= 91 && (int)message.charAt(i) <= 96) {
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 96) {
+					encryptMessage += (char)(newAsciiCode - 6);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			//for lowercases
+			}else if((int)message.charAt(i) >= 97 && (int)message.charAt(i) <= 122) {
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 122) {
+					encryptMessage += (char)(newAsciiCode - 26);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			//for special chars (after "z")
+			}else if((int)message.charAt(i) >= 123 && (int)message.charAt(i) <= 126) {
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 126) {
+					encryptMessage += (char)(newAsciiCode - 4);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			//for latins chars
+			} else if((int)message.charAt(i) >= 128 && (int)message.charAt(i) <= 255){
+				int newAsciiCode = (int)message.charAt(i) + key;
+				if(newAsciiCode > 255) {
+					encryptMessage += (char)(newAsciiCode - 128);
+				} else {
+					encryptMessage += (char)newAsciiCode;
+				}
+			//for any other chars (no change)
+			} else {
+				encryptMessage += message.charAt(i);
 			}
 		}
 		
-
+		
 		System.out.println(encryptMessage);
 	}
 	
 	/**
-	 * decrypt a message
+	 * decrypt message (works for ASCII and Latin encoding)
 	 * @param message
 	 * @param key
 	 */
 	static void decrypt(String message, int key) {
-		// on créer la variable qui va contenir le texte déchiffré
+		
 		String decryptMessage = "";
-
 		
-		//on parcours le texte à déchiffrer
-		for (int i = 0; i < message.length(); i++) {
-			// on gère le cas où le caractère est un espace
-			if ((int)message.charAt(i) == 32) {
-				decryptMessage += (char)32;
-			//on gère le cas où l'on est entre l'espace et les majuscules dans la table ASCII (soit 32 < c < 65)
-			} else if ( ((int)message.charAt(i) - key) > 32 && ((int)message.charAt(i) < 65)) {
-				int temp = ((int)message.charAt(i) - key) - 122;
-				decryptMessage += (char)(temp + 96);
-				// les lettres minuscules commences à 97 dans la table ASCII
-		
-			//on gère le cas ou on n'est entre les lettres majuscules et les lettres minuscules dans la table ASCII (soit pour 90 < c < 97)
-			} else if (((int)message.charAt(i) - key) > 90 && (int)message.charAt(i) < 97) {
-				int temp = (((int)message.charAt(i) - key) - 90);
-				decryptMessage += (char)(temp + 64); //les lettres majuscules commences à 65 dans la table ASCII
+		for(int i = 0; i < message.length(); i++) {
+			//for "space" (no change)
+			if((int)message.charAt(i) == 32) {
+				decryptMessage += message.charAt(i);
+				
+			//for special chars (after "space" and before uppercases)
+			}else if((int)message.charAt(i) >= 33 && (int)message.charAt(i) <= 64) {
+				int newAsciiCode = (int)message.charAt(i) - key;
+				if(newAsciiCode < 33) {
+					decryptMessage += (char)(newAsciiCode + 32);
+				} else {
+					decryptMessage += (char)newAsciiCode;
+				}
+			//for uppercases
+			}else if((int)message.charAt(i) >= 65 && (int)message.charAt(i) <= 90) {
+				int newAsciiCode = (int)message.charAt(i) - key;
+				if(newAsciiCode < 65) {
+					decryptMessage += (char)(newAsciiCode + 26);
+				} else {
+					decryptMessage += (char)newAsciiCode;
+				}
+			//for special chars (after uppercases and before lowercases)
+			}else if((int)message.charAt(i) >= 91 && (int)message.charAt(i) <= 96) {
+				int newAsciiCode = (int)message.charAt(i) - key;
+				if(newAsciiCode < 91) {
+					decryptMessage += (char)(newAsciiCode + 6);
+				} else {
+					decryptMessage += (char)newAsciiCode;
+				}
+			//for lowercases
+			}else if((int)message.charAt(i) >= 97 && (int)message.charAt(i) <= 122) {
+				int newAsciiCode = (int)message.charAt(i) - key;
+				if(newAsciiCode < 97) {
+					decryptMessage += (char)(newAsciiCode + 26);
+				} else {
+					decryptMessage += (char)newAsciiCode;
+				}
+			//for latins chars
+			} else if((int)message.charAt(i) >= 128 && (int)message.charAt(i) <= 255){
+				int newAsciiCode = (int)message.charAt(i) - key;
+				if(newAsciiCode < 128) {
+					decryptMessage += (char)(newAsciiCode + 128);
+				} else {
+					decryptMessage += (char)newAsciiCode;
+				}
+			//for any other chars (no change)
 			} else {
-				decryptMessage += (char)((int)message.charAt(i) - key);
+				decryptMessage += message.charAt(i);
 			}
 		}
 		
-
+		
 		System.out.println(decryptMessage);
 	}
 }
